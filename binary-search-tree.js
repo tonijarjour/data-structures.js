bbf7be83d37fb8efe217f4e92b2695d6a3136816
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2022-07-02 21:30:13 -0400

    bst fixed

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,6 +1,6 @@
 import Queue from "./queue.js";
 
-class TreeElement {
+class Element {
   left;
   right;
 
@@ -25,141 +25,97 @@ export default class BinarySearchTree {
     return this.#size;
   }
 
-  contains(value, origin = this.#root) {
-    if (!origin) return false;
-
-    let currentElement = origin;
-
-    if (value === currentElement.value) return true;
-
-    while (currentElement.left || currentElement.right) {
-      currentElement =
-        value < currentElement.value
-          ? currentElement.left
-          : currentElement.right;
-
-      if (value === currentElement.value) return true;
-
-      if (!currentElement.left && value < currentElement.value) return false;
-      if (!currentElement.right && value > currentElement.value) return false;
-    }
-    return false;
-  }
-
-  add(value) {
-    const newTreeElement = new TreeElement(value);
+  insert(value) {
+    if (!value) return false;
 
     if (this.#size === 0) {
-      this.#root = newTreeElement;
+      this.#root = new Element(value);
+      this.#root.balanceFactor = 0;
       return (this.#size = 1);
     }
 
+    const newElement = new Element(value);
     let currentElement = this.#root;
 
     while (true) {
-      if (value === currentElement.value) return;
+      if (currentElement.value === value) return false;
 
-      if (!currentElement.left && value < currentElement.value) {
-        currentElement.left = newTreeElement;
-        return ++this.#size;
-      } else if (!currentElement.right && value > currentElement.value) {
-        currentElement.right = newTreeElement;
-        return ++this.#size;
+      if (value < currentElement.value) {
+        if (!currentElement.left) {
+          currentElement.left = newElement;
+          break;
+        } else {
+          currentElement = currentElement.left;
+        }
+      } else if (value > currentElement.value) {
+        if (!currentElement.right) {
+          currentElement.right = newElement;
+          break;
+        } else {
+          currentElement = currentElement.right;
+        }
       }
-
-      if (value > currentElement.value) currentElement = currentElement.right;
-      else if (value < currentElement.value)
-        currentElement = currentElement.left;
-    }
-  }
-
-  #find(value) {
-    let returnObject = { remove: null, previous: null, direction: null };
-    if (this.#root.value === value) {
-      returnObject.remove = this.#root;
-      return returnObject;
-    }
-    let currentElement = this.#root;
-    let previousElement;
-
-    while (currentElement.left || currentElement.right) {
-      returnObject.previous = currentElement;
-      if (currentElement.left && currentElement.left.value === value) {
-        returnObject.remove = currentElement.left;
-        returnObject.direction = -1;
-        return returnObject;
-      } else if (currentElement.right && currentElement.right.value === value) {
-        returnObject.remove = currentElement.right;
-        returnObject.direction = 1;
-        return returnObject;
-      }
-
-      previousElement = currentElement;
-      [currentElement, returnObject.direction] =
-        value < currentElement.value
-          ? [currentElement.left, -1]
-          : [currentElement.right, 1];
     }
 
-    if (currentElement.value === value) {
-      returnObject.remove = currentElement;
-      returnObject.previous = previousElement;
-      return returnObject;
-    }
-
-    throw Error("not found");
+    ++this.#size;
   }
 
   remove(value) {
-    if (this.#size === 0) {
-      throw Error("empty tree");
-    }
-    if (this.#size === 1) {
-      if (this.#root.value === value) {
+    if (!value) return false;
+
+    if (this.#root.value === value) {
+      if (this.#size === 1) {
         this.#root = undefined;
         return (this.#size = 0);
-      } else {
-        throw Error("not found");
+      } else if (!(this.#root.left && this.#root.right)) {
+        this.#root = this.#root.left ? this.#root.left : this.#root.right;
+        return --this.#size;
       }
     }
 
-    let { remove, previous, direction } = this.#find(value);
+    let current = this.#root;
+    let previous;
+
+    while (value < current.value || value > current.value) {
+      previous = current;
+      if (value < current.value) current = current.left;
+      if (value > current.value) current = current.right;
+    }
+
+    let side;
+    if (previous) side = previous.left === current ? -1 : 1;
 
     // leaf
-    if (!remove.left && !remove.right) {
-      if (direction === -1) {
-        previous.left = remove.value = undefined;
+    if (!current.left && !current.right) {
+      if (side === -1) {
+        previous.left = undefined;
+      } else if (side === 1) {
+        previous.right = undefined;
       }
-      if (direction === 1) {
-        previous.right = remove.value = undefined;
-      }
+      current.value = undefined;
     }
 
-    // one way branch
-    if (!(remove.left && remove.right)) {
-      if (direction === -1) {
-        if (remove.left) {
-          previous.left = remove.left;
-          remove.value = remove.left = undefined;
+    // one way
+    if (!(current.left && current.right)) {
+      if (side === -1) {
+        if (current.left) {
+          previous.left = current.left;
         } else {
-          previous.left = remove.right;
-          remove.value = remove.right = undefined;
+          previous.left = current.right;
+        }
+      } else {
+        if (current.left) {
+          previous.right = current.left;
+        } else {
+          previous.right = current.right;
         }
       }
-      if (direction === 1) {
-        if (remove.left) {
-          previous.right = remove.left;
-          remove.value = remove.left = undefined;
-        } else {
-          previous.right = remove.right;
-          remove.value = remove.right = undefined;
-        }
-      }
+      current.left = current.right = undefined;
     }
 
     // full branch
-    if (remove.left && remove.right) {
-      let replace = remove.right;
+    if (current.left && current.right) {
+      let replace = current.right;
       previous = undefined;
 
       while (replace.left) {
@@ -167,17 +123,16 @@ export default class BinarySearchTree {
         replace = replace.left;
       }
 
-      remove.value = replace.value;
+      current.value = replace.value;
 
       if (!previous) {
-        remove.right = replace.right;
+        current.right = replace.right;
       } else {
         previous.left = replace.right;
       }
 
-      replace.value = replace.right = undefined;
+      replace.right = replace.value = undefined;
     }
-
     --this.#size;
   }
 
@@ -213,3 +168,15 @@ export default class BinarySearchTree {
     }
   }
 }
+
+let tree = new BinarySearchTree();
+
+tree.insert(500);
+tree.insert(400);
+tree.insert(600);
+tree.insert(350);
+tree.insert(450);
+tree.insert(550);
+tree.insert(650);
+
+tree.postOrder();
